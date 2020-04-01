9c6389288a199736a9db2f475bccc1123af461c5
Andrey Shilov <andrey.dry7.shilov@yandex.ru>
2020-04-01 22:10:07 +0300

    Add declare(strict_types=1); to files

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $a = 1;
     return view('welcome');
 });
