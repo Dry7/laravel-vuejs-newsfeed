02ea3a2b942bd31c9ded142ac146eb9497076112
Andrey Shilov <andrey.dry7.shilov@yandex.ru>
2020-04-01 19:02:36 +0300

    Add Category model

---
+++ app/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Category extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    public function feeds(): BelongsToMany
+    {
+        return $this->belongsToMany(Feed::class, 'feed_categories');
+    }
+}
