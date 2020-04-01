acd2cc361d52570a4d178f3a351bfe3ef5e51666
Andrey Shilov <andrey.dry7.shilov@yandex.ru>
2020-04-01 23:00:27 +0300

    Add FeedView

---
+++ app/Http/Views/FeedView.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Views;
+
+use App\Models\Category;
+use App\Models\Feed;
+use App\Models\Media;
+use Illuminate\Support\Collection;
+
+class FeedView implements \JsonSerializable
+{
+    private Feed $feed;
+
+    public function __construct(Feed $feed)
+    {
+        $this->feed = $feed;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->feed->id,
+            'title' => $this->feed->title,
+            'slug' => $this->feed->slug,
+            'content' => $this->feed->content,
+            'categories' => $this->categories(),
+            'media' => $this->media(),
+        ];
+    }
+
+    private function categories(): array
+    {
+        $categories = $this->feed->categories()->withPivot(['primary'])->get();
+        $primary = $categories->where('pivot.primary', true)->first();
+
+        return [
+            'primary' => $primary ? $primary->name : null,
+            'additional' => $categories
+                ->where('pivot.primary', false)
+                ->map(static fn (Category $category) => $category->name)
+                ->values()
+        ];
+    }
+
+    private function media(): Collection
+    {
+        return $this->feed->media->map(static fn (Media $media) => new MediaView($media))->values();
+    }
+}
