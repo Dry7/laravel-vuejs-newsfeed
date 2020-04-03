422b3298bd82470981de0083a0758c518d932274
Andrey Shilov <andrey.dry7.shilov@yandex.ru>
2020-04-03 13:04:20 +0300

    Add search filters to FeedService

--- frontend/src/services/FeedService.ts
+++ frontend/src/services/FeedService.ts
@@ -4,8 +4,19 @@ import { Observable } from 'rxjs';
 class FeedService {
   endpoint = 'http://localhost:8282/api/v1/';
 
-  all(): Observable<Response> {
-    return fromFetch(`${this.endpoint}search`, { method: 'POST' });
+  all(category: number | null, query: string | null): Observable<Response> {
+    return fromFetch(`${this.endpoint}search?${FeedService.buildQuery(category, query)}`, { method: 'POST' });
+  }
+
+  private static buildQuery(category: number | null, query: string | null): string {
+    const url = [];
+    if (category !== null && category > 0) {
+      url.push(`category=${category}`);
+    }
+    if (query !== null && query.length > 0) {
+      url.push(`query=${encodeURIComponent(query)}`);
+    }
+    return url.join('&');
   }
 }
 
