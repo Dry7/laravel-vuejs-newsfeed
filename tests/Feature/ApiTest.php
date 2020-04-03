5eaf76ce38f5ee1b9b017ef0bfc2ec4e26c836d0
Andrey Shilov <andrey.dry7.shilov@yandex.ru>
2020-04-03 20:44:03 +0300

    Functional api tests

---
+++ tests/Feature/ApiTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class ApiTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testCategories(): void
+    {
+        factory(Category::class, 20)->create();
+
+        $this
+            ->post('/api/v1/categories')
+            ->assertOk()
+            ->assertJsonCount(20, 'items')
+            ->assertJsonPath('total', 20);
+    }
+
+    public function testSearch(): void
+    {
+        $this
+            ->postJson('/api/v1/search', [])
+            ->assertOk()
+            ->assertJson([
+                'items' => [],
+                'total' => 0,
+            ]);
+    }
+
+    public function testSearchWithInvalidRequest(): void
+    {
+        $this
+            ->postJson('/api/v1/search', [
+                'category' => 'wrong',
+            ])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}
